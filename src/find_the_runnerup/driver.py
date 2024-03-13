fb80288c0f059b4e196a2b3549c92816e6d4273e
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 11:04:12 +0530

    Found the runnerup scores

--- src/find_the_runnerup/driver.py
+++ src/find_the_runnerup/driver.py
@@ -1,7 +1,7 @@
 from util import find_runner_up_score
 import logging
 n = int(input("enter the number:"))
-scores = list(map(int, input().split()))
+scores = list(map(int, input("enter the input:").split()))
 
-runner_up_score = find_runner_up_score(scores)
+runner_up_score = find_runner_up_score(n,scores)
 logging.debug(runner_up_score)
