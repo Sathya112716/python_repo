64c42b4e7d497043473602ca1dc7a89f60e3675d
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 10:22:01 +0530

    find the runnerup by map reduce function

--- src/find_the_runnerup/driver.py
+++ src/find_the_runnerup/driver.py
@@ -1,1 +1,7 @@
+from util import find_runner_up_score
+import logging
+n = int(input("enter the number:"))
+scores = list(map(int, input().split()))
 
+runner_up_score = find_runner_up_score(scores)
+logging.debug(runner_up_score)
--- src/find_the_runnerup/util.py
+++ src/find_the_runnerup/util.py
@@ -1,1 +1,6 @@
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def find_runner_up_score(scores):
+    unique_scores = sorted(set(scores), reverse=True)
+    return unique_scores[1]
