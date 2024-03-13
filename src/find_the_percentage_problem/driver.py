a945942930da78d2804e351409834c7a7e7afb00
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 07:13:14 +0530

    Calculated average for student records

--- src/find_the_percentage_problem/driver.py
+++ src/find_the_percentage_problem/driver.py
@@ -1,1 +1,5 @@
+from util import calc_average
+import logging
 
+result = calc_average(3,['sathya 30 40 50','priya 45 65 75','neha 88 78 98'],'sathya')
+logging.debug(result)
--- src/find_the_percentage_problem/util.py
+++ src/find_the_percentage_problem/util.py
@@ -1,1 +1,19 @@
+# util.py
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def calc_average(n,student_input,query_name):
+    student_marks={}
+    for _ in range(n):
+        name, *marks = student_input[_].split()
+        scored_value= list(map(float, marks))
+        student_marks[name] = scored_value
+
+    if query_name in student_marks:
+        average = sum(student_marks[query_name]) / len(student_marks[query_name])
+        return f"{average:.2f}"
+    else:
+        return "Student not found"
 
