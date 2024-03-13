227efa0ba6dddba172c4c81764f2f6b318796550
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 19:06:05 +0530

    Merging the string by removing the duplicates

--- src/merge_the_tools/util.py
+++ src/merge_the_tools/util.py
@@ -1,1 +1,17 @@
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def merge_the_tools(string, k):
+    n = len(string)
+    substrings = [string[i:i + k] for i in range(0, n, k)]
+
+    for substring in substrings:
+        unique_chars = []
+        for char in substring:
+            if char not in unique_chars:
+                unique_chars.append(char)
+        result=''.join(unique_chars)
+        logging.debug(result)
+    return(result)
