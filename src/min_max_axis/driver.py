cd4f1b4e562cf3fa8e2d0c1bc36f26c47c49f5d9
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 16:55:40 +0530

    Numpy Module

--- src/min_max_axis/driver.py
+++ src/min_max_axis/driver.py
@@ -1,1 +1,14 @@
+import logging
+import numpy as np
+
+from util import min_max_axis
+
+n, m = map(int, input('enter the numbers(n m)').split())
+arr = []
+for _ in range(n):
+        row = list(map(int, input().split()))
+        arr.append(row)
+axis = int(input("Enter axis (0 for columns, 1 for rows, None for overall): "))
+result = min_max_axis(arr,axis)
+logging.debug(result)
 
--- src/min_max_axis/util.py
+++ src/min_max_axis/util.py
@@ -1,1 +1,12 @@
 
+import logging
+import numpy as np
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def min_max_axis(arr,axis):
+    array= np.array(arr)
+    min_axis = np.min(array,axis=axis)
+    return np.max(min_axis)
+
+
