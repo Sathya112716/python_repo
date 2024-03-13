c1f2973ce8ae27d912fbea5c24b9a2e7d2705f38
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 18:03:31 +0530

    Mutate Functions using User Defined Funxtions

--- src/mutate_string/driver.py
+++ src/mutate_string/driver.py
@@ -1,1 +1,6 @@
+import logging
 
+from util import mutate_string
+
+result = mutate_string("sathya",4,'u')
+logging.info(result)
--- src/mutate_string/util.py
+++ src/mutate_string/util.py
@@ -1,1 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def mutate_string(s, position, character):
+    s = input("Enter the string").strip()
+    position, character = input("Enter the character and position").split()
+    position = int(position)
+
+    return s[:position] + character + s[position + 1:]
+
+
+
 
