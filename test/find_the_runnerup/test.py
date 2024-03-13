83d166b1871f601d3e2a8c601bceac00a49a06aa
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-13 11:07:30 +0530

    added the test cases

--- test/find_the_runnerup/test.py
+++ test/find_the_runnerup/test.py
@@ -1,1 +1,30 @@
 
+import unittest
+from python_repo.src.find_the_runnerup.util import find_runner_up_score
+
+
+class TestCalAvg(unittest.TestCase):
+    def test_find_runner_up_score(self):
+        n = 3
+        scores = [4,6,1,7,9]
+        actualoutput = find_runner_up_score(scores)
+        expectedoutput = 7
+        self.assertEqual(actualoutput, expectedoutput)
+
+    def test1_find_the_runnerup(self):
+        n = 6
+        scores=[8,4,5,2,9,6]
+        actualoutput = find_runner_up_score(scores)
+        expectedoutput = 8
+        self.assertEqual(actualoutput, expectedoutput)
+
+    def test2_find_the_runnerup(self):
+        n = 3
+        scores = [4,1,5,8,10]
+        actualoutput = find_runner_up_score(scores)
+        expectedoutput = 8
+        self.assertEqual(actualoutput, expectedoutput)
+
+
+if __name__ == '__main__':
+    unittest.main()
