a907f0d256dd072d741bd5ae23860609ed88c359
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-14 08:00:24 +0530

    Formatting numbers are printed

--- src/python_string_formatted/driver.py
+++ src/python_string_formatted/driver.py
@@ -1,1 +1,8 @@
+import logging
 
+from util import print_formatted
+
+value = [17]
+for number in value:
+    result=print_formatted(17)
+    logging.debug(result)
--- src/python_string_formatted/util.py
+++ src/python_string_formatted/util.py
@@ -1,1 +1,17 @@
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def print_formatted(number):
+    width = len(bin(number)[2:])
+    for i in range(1, number + 1):
+        decimal = str(i)
+        octal = oct(i)[2:]
+        hexadecimal = hex(i)[2:].upper()
+        binary = bin(i)[2:]
+
+        formatted_decimal = decimal.rjust(width)
+        formatted_octal = octal.rjust(width)
+        formatted_hexadecimal = hexadecimal.rjust(width)
+        formatted_binary = binary.rjust(width)
+
+        logging.debug(f"{formatted_binary} {formatted_octal}{formatted_hexadecimal}{formatted_decimal}")
