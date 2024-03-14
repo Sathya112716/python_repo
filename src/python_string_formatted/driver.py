fcf112000b3f6bb7c15a5143fdd0522fbba8e874
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-14 12:28:49 +0530

    Print the formatt type required for given number

--- src/python_string_formatted/driver.py
+++ src/python_string_formatted/driver.py
@@ -4,5 +4,5 @@ from util import print_formatted
 
 value = [17]
 for number in value:
-    result=print_formatted(17)
-    logging.debug(result)
+    rslt=print_formatted(17)
+    logging.debug(rslt)
