1cf555b65881e47c90fe4a2e90a0bfceabb95488
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-14 10:56:02 +0530

    Calendar module

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -2,10 +2,10 @@ from util import find_day
 
 import logging
 
-date_input = input("Enter the date (MM DD YYYY): ")
+date_input =input("Enter the date (MM DD YYYY): ")
 month, day, year = map(int, date_input.split())
-day_of_week = find_day(month, day, year)
-logging.debug("The day on {} {} {} is: {}".format(month, day, year, day_of_week))
+day_of_the_week = find_day(month, day, year)
+logging.debug("The day on {} {} {} is: {}".format(month, day, year, day_of_the_week))
 
 
 
