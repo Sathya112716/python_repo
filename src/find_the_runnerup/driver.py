eb56ca3ec312b4cb0788f7e02f51944e47add18c
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-14 12:07:08 +0530

    For the given input finding the runnerup

--- src/find_the_runnerup/driver.py
+++ src/find_the_runnerup/driver.py
@@ -1,5 +1,5 @@
-from util import find_runner_up_score
 import logging
+from util import find_runner_up_score
 n = int(input("enter the number:"))
 scores = list(map(int, input("enter the input:").split()))
 
--- src/find_the_runnerup/util.py
+++ src/find_the_runnerup/util.py
@@ -2,5 +2,5 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 def find_runner_up_score(n,scores):
-    unique_scores = sorted(set(scores), reverse=True)
-    return unique_scores[1]
+    uniq_scores = sorted(set(scores), reverse=True)
+    return uniq_scores[1]
