7fa7ad649759296ac4fcd599adca65a17340235d
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-14 08:19:14 +0530

    Find the day by using calendar module

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -1,1 +1,11 @@
+from util import find_day
+
+import logging
+
+date_input = input("Enter the date (MM DD YYYY): ")
+month, day, year = map(int, date_input.split())
+day_of_week = find_day(month, day, year)
+logging.debug("The day on {} {} {} is: {}".format(month, day, year, day_of_week))
+
+
 
--- src/calendar_module/util.py
+++ src/calendar_module/util.py
@@ -1,1 +1,8 @@
+import calendar
 
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def find_day(month, day, year):
+    weekday_index = calendar.weekday(year, month, day)
+    return calendar.day_name[weekday_index].upper()
