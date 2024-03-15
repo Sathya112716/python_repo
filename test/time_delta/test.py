bf718f973f36434c20db542a6f0c2cfacf3e9d3a
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-15 18:36:29 +0530

    Get the difference for the time

--- test/time_delta/test.py
+++ test/time_delta/test.py
@@ -4,7 +4,7 @@ from python_repo.src.time_delta.util import get_time_difference
 
 
 class MyTestCase(unittest.TestCase):
-    def test_time_delta(self):
+    def test1_time_delta(self):
         self.assertEqual( get_time_difference("2\nSun 10 May 2015 13:54:36 -0700\nSun 10 May 2015 13:54:36 -0000\nSat 02 May 2015 19:54:36 +0530\nFri 01 May 2015 13:54:36 -0000"),'25200\n88200')  # add assertion here
 
  # add assertion here
