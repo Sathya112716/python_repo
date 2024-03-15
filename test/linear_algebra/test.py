278b3b64156db7a43594a79264f20fda8164d408
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-15 18:28:55 +0530

    For the given input and matrix calculate

--- test/linear_algebra/test.py
+++ test/linear_algebra/test.py
@@ -2,7 +2,7 @@ import unittest
 from python_repo.src.linear_algebra.util import calculate_determinant
 
 class MyTestCase(unittest.TestCase):
-    def test_find_the_determinant_value(self):
+    def test1_find_the_determinant_value(self):
         N = 2
         matrix_value = [[1.1, 1.1], [1.1, 1.1]]
         result = calculate_determinant(N, matrix_value)
