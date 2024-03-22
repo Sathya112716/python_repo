19ecbb2defa4b808578738a964184041b30cef9e
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-22 08:52:04 +0530

    Length of cube given pick the left or rightmost one

--- test/Piling_up/test.py
+++ test/Piling_up/test.py
@@ -2,7 +2,7 @@ import unittest
 from python_repo.src.Piling_up.util import can_stack_cubes
 
 class TestCanStackCubes(unittest.TestCase):
-    def can_stack_cubes(self):
+    def can_stack_cube(self):
         self.assertEqual(can_stack_cubes(6, [4, 3, 2, 1, 3, 4]), "Yes")
         self.assertEqual(can_stack_cubes(3, [1, 3, 4]), "No")
 
