70b979eaa7ece72e6685791066be510041017ba4
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-22 15:18:45 +0530

    For the given string mutate the character

--- test/mutate_string/test.py
+++ test/mutate_string/test.py
@@ -1,23 +1,10 @@
-
 import unittest
+import logging
 from python_repo.src.mutate_string.util import mutate_string
-
-
-class TestCalAvg(unittest.TestCase):
-    def test_mutate_string(self):
-        actualoutput = mutate_string("sathya",2,'u')
-        expectedoutput = "suthya"
-        self.assertEqual(actualoutput, expectedoutput) ##second position is mutated
-
+class MyTestCase(unittest.TestCase):
     def test1_mutate_string(self):
-        actualoutput = mutate_string("ravi", 4,'l')
-        expectedoutput = "ravl"
-        self.assertEqual(actualoutput, expectedoutput) ##4th position
+        self.assertEqual(mutate_string("sathya",2,"u"),"suthya") ##second position is mutated)
 
-    def test_mutate_string(self):
-        actualoutput = mutate_string("ambika", 6,'y')
-        expectedoutput = "ambiky"
-        self.assertEqual(actualoutput, expectedoutput) #6th posotion
 
 if __name__ == '__main__':
     unittest.main()
