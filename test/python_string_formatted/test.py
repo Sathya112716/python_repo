7165a02799ceadf4569d546fc3e8a1aa55bea968
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:28:38 +0530

    test cases for python string formatted

--- test/python_string_formatted/test.py
+++ test/python_string_formatted/test.py
@@ -1,11 +1,13 @@
-
 import unittest
 from python_repo.src.python_string_formatted.util import print_formatted
 
 
 class TestCase(unittest.TestCase):
-    def print_formatted(self):
-        self.assertEqual(print_formatted(4), '  1   1   1   1\n  2   2   2  10\n  3   3   3  11\n  4   4   4 100')
+    def test1(self):
+        # input =17
+        actual_output = print_formatted()
+        expected_output =17 21 11 10001
+        self.assertEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
