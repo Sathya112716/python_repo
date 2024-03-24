d18a96ff472dcaca428cfc843fe32af2aed53674
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 23:30:16 +0530

    iterates through different sections of the logo, including the top cone, top pillars, middle belt, bottom pillars, and bottom cone, printing each section accordingly

--- src/text_alignment/driver.py
+++ src/text_alignment/driver.py
@@ -1,5 +1,2 @@
-import logging
-
-from util import text_alignment_logo
-thickness = int(input())  # This must be an odd number
-logging.debug(text_alignment_logo(thickness))
+from util import print_hackerrank_logo
+print_hackerrank_logo()
