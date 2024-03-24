8256afc27d980afb49c08304af467aa35199fc14
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 22:22:15 +0530

    Find the second maximum number for the list

--- src/find_the_runnerup/driver.py
+++ src/find_the_runnerup/driver.py
@@ -1,7 +1,3 @@
-import logging
-from python_repo.src.find_the_runnerup.util import find_runner_up_score
+from util import find_runner_up_score
+find_runner_up_score()
 
-n = int(input("enter the number:"))
-scores = list(map(int, input("enter the input:").split()))
-
-logging.debug(find_runner_up_score(n,scores))
--- src/find_the_runnerup/util.py
+++ src/find_the_runnerup/util.py
@@ -1,8 +1,11 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-def find_runner_up_score(n,scores):
+def find_runner_up_score():
+    n = int(input("enter the number:"))
+    scores = list(map(int, input("enter the input:").split()))
+
     uniq_scores = sorted(set(scores), reverse=True)
     return uniq_scores[1]  if len(uniq_scores) > 1 else "No second maximum found"
-
+logging.debug(find_runner_up_score())
 
