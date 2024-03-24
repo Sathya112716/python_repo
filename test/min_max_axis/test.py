acd87e76af89d1478315e44c71608d1ed3f33efb
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:22:50 +0530

    test cases for max of min axis

--- test/min_max_axis/test.py
+++ test/min_max_axis/test.py
@@ -1,14 +1,23 @@
 import unittest
-from python_repo.src.min_max_axis.util import min_max_axis
+from python_repo.src.min_max_axis.util import min_max
 
 class TestMinimumMaximum(unittest.TestCase):
     def test1_minimum_maximum(self):
-        arr = [[2, 5],
-               [3, 7],
-               [1, 3],
-               [4, 0]]
-        result = min_max_axis(arr)
-        self.assertEqual(result, 3)  # Correct the expected result to 3
+        #arr = [[2, 5], [3, 7], [1, 3], [4, 0]]  # Define the input array
+        actual_output = min_max()  # Call min_max function with the input array
+        expected_output = 3
+        self.assertEqual(actual_output, expected_output)
+
+    def test2_minimum_maximum(self):
+        #arr = [[3, 5], [3, 4], [1, 3], [4, 0]]  # Define the input array
+        actual_output = min_max()  # Call min_max function with the input array
+        expected_output = 3
+        self.assertEqual(actual_output, expected_output)
+    def test3_minimum_maximum(self):
+        #arr = [[2, 3], [1, 2], [1, 3]]  # Define the input array
+        actual_output = min_max()  # Call min_max function with the input array
+        expected_output = 1
+        self.assertEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
