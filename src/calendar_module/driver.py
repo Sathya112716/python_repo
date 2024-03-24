ec64246779658e7860e9c63bcdc6920fb9504807
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 21:56:11 +0530

    Import datetime module and find the day for the input

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -1,11 +1,8 @@
 from util import find_day
 
-import logging
+find_day()
 
-date_input =input("Enter the date (MM DD YYYY): ")
-month, day, year = map(int, date_input.split())
-day_of_the_week = find_day(month, day, year)
-logging.debug("The day on {} {} {} is: {}".format(month, day, year, day_of_the_week))
+
 
 
 
--- src/calendar_module/util.py
+++ src/calendar_module/util.py
@@ -1,8 +1,13 @@
 import calendar
-
+import datetime
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def find_day(month, day, year):
-    weekday_index = calendar.weekday(year, month, day)
-    return calendar.day_name[weekday_index].upper()
+def find_day():
+    date_input=input("Enter the date:")
+    month, day, year = map(int, date_input.split())
+    day_of_the_week = datetime.date(year, month, day).weekday()
+    days=['Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday','Sunday']
+    logging.debug(days[day_of_the_week])
+
+    return days[day_of_the_week].upper()
