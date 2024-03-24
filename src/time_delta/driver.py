1cc775fa92485f9edf37429ba027473fd787e9e0
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 23:33:01 +0530

    calculates the absolute time difference in seconds and yields the result

--- src/time_delta/driver.py
+++ src/time_delta/driver.py
@@ -1,10 +1,5 @@
-import logging
+
 from util import get_time_difference
+get_time_difference()
 
-
-n = int(input())
-for _ in range(n):
-    timestamp1 = input().strip()
-    timestamp2 = input().strip()
-    logging.debug(get_time_difference(timestamp1, timestamp2))
 
--- src/time_delta/util.py
+++ src/time_delta/util.py
@@ -1,10 +1,23 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-from datetime import datetime, timedelta
+from datetime import datetime
 
-def get_time_difference(timestamp1, timestamp2):
-    format_str = "%a %d %b %Y %H:%M:%S %z"
-    time1 = datetime.strptime(timestamp1, format_str)
-    time2 = datetime.strptime(timestamp2, format_str)
-    time_diff = abs(time1 - time2)
-    return int(time_diff.total_seconds())
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def get_time_difference():
+    num_testcases = int(input().strip())
+    for _ in range(num_testcases):
+        timestamp1 = input().strip()
+        timestamp2 = input().strip()
+
+        date_format = "%a %d %b %Y %H:%M:%S %z"
+        time1 = datetime.strptime(timestamp1, date_format)
+        time2 = datetime.strptime(timestamp2, date_format)
+
+        time_difference = abs((time1 - time2).total_seconds())
+        yield int(time_difference)
+for time_difference in get_time_difference():
+    logging.debug(time_difference)
+
+
+
