dd1611f6334dda301119bfd01e7fda5f88a8a985
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:27:00 +0530

    test cases for piling up

--- test/piling_up/test.py
+++ test/piling_up/test.py
@@ -1,10 +1,25 @@
 import unittest
-from python_repo.src.Piling_up.util import can_stack_cubes
+from python_repo.src.piling_up.util import can_stack_cubes
 
 class TestCanStackCubes(unittest.TestCase):
-    def can_stack_cube(self):
-        self.assertEqual(can_stack_cubes(6, [4, 3, 2, 1, 3, 4]), "Yes")
-        self.assertEqual(can_stack_cubes(3, [1, 3, 4]), "No")
+    def test1(self):
+        # 2
+        # 6
+        #4 3 2 1 3 4
+        # 3
+        # 1 3 2
+        actual_output=can_stack_cubes()
+        expected_output='No'
+        self.assertEqual(actual_output,expected_output)
 
+    def test2(self):
+        # 1
+        # 5
+        #1 2 3 4 5
+        # 2
+        # 1 4 5
+        actual_output=can_stack_cubes()
+        expected_output='Yes'
+        self.assertEqual(actual_output,expected_output)
 if __name__ == '__main__':
     unittest.main()
