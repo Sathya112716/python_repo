afc25e920534697682120f5a25791accaccb0e14
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 23:01:36 +0530

    converts the position to an integer and replaces the character at that position in the string s with the specified character.

--- src/mutate_string/driver.py
+++ src/mutate_string/driver.py
@@ -1,5 +1,5 @@
-import logging
+
 
 from util import mutate_string
 
-logging.debug(mutate_string("sathya",2,'u'))
+mutate_string()
--- src/mutate_string/util.py
+++ src/mutate_string/util.py
@@ -1,12 +1,13 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-def mutate_string(s, position, character):
+def mutate_string():
     s = input("").strip()
     position, character = input("").split()
     position = int(position)
 
     return s[:position] + character + s[position + 1:]
+logging.debug(mutate_string())
 
 
 
