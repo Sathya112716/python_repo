85317afa280c54cd7d7f03f91cc635d54f5bfc41
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:14:32 +0530

    test cases for iterables and iterators

--- test/iterables_iterators/test.py
+++ test/iterables_iterators/test.py
@@ -1,10 +1,23 @@
 import unittest
-from python_repo.src.Iterables_Iterators.util import probability_of_letter
+from python_repo.src.iterables_iterators.util import probability_of_letter
 
 class TestProbability(unittest.TestCase):
     def test_case_1(self):
-        result = probability_of_letter(4, ['s', 'a', 't', 'r'], 2)
-        self.assertEqual(result, 0.8333, places=4)
+        #indices=4,letters=aacd,k=2
+        actual_output= probability_of_letter()
+        expected_output="0.125"
+        self.assertEqual(actual_output,expected_output)
 
+    def test_case_2(self):
+        #indices=5,letters=sghig,k=3
+        actual_output= probability_of_letter()
+        expected_output="0.024"
+        self.assertEqual(actual_output,expected_output)
+
+    def test_case_3(self):
+        #indices=5,letters=cdefg,k=2
+        actual_output= probability_of_letter()
+        expected_output="0.08"
+        self.assertEqual(actual_output,expected_output)
 if __name__ == "__main__":
     unittest.main()
