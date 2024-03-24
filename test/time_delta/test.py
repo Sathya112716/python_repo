c6e10286d8596b6029056348b9c3bc9ed98d6ffd
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:31:11 +0530

    test cases for time delta

--- test/time_delta/test.py
+++ test/time_delta/test.py
@@ -4,9 +4,12 @@ from python_repo.src.time_delta.util import get_time_difference
 
 
 class MyTestCase(unittest.TestCase):
-    def test1_time_delta(self):
-        self.assertEqual( get_time_difference("2\nSun 10 May 2015 13:54:36 -0700\nSun 10 May 2015 13:54:36 -0000\nSat 02 May 2015 19:54:36 +0530\nFri 01 May 2015 13:54:36 -0000"),'25200\n88200')  # add assertion here
-
+    def test1(self):
+        #n=2
+        #time1,time2,timestamp1,timestamp2="Sun 10 May 2015 13:54:36 -0700,Sun 10 May 2015 13:54:36 -0000,Sat 02 May 2015 19:54:36 +0530,Fri 01 May 2015 13:54:36 -0000
+        actual_output=get_time_difference()
+        expected_output=(25200,88200)
+        self.assertEqual(actual_output,expected_output)
  # add assertion here
 
 
