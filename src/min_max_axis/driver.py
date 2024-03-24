8510eb85d566e57e785f07872ceb8dd172c0b68c
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 22:58:22 +0530

    finds the maximum of these minimum values. Finally, it logs and returns the maximum value

--- src/min_max_axis/driver.py
+++ src/min_max_axis/driver.py
@@ -1,14 +1,5 @@
-import logging
-import numpy as np
 
-from util import min_max_axis
+from util import min_max
 
-n, m = map(int, input('enter the numbers(n m)').split())
-arr = []
-for _ in range(n):
-        row = list(map(int, input().split()))
-        arr.append(row)
-axis = int(input("Enter axis (0 for columns, 1 for rows, None for overall): "))
-rslt = min_max_axis(arr,axis)
-logging.debug(rslt)
+min_max()
 
--- src/min_max_axis/util.py
+++ src/min_max_axis/util.py
@@ -4,9 +4,19 @@ import numpy as np
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-def min_max_axis(arr,axis):
-    array= np.array(arr)
-    min_axis = np.min(array,axis=axis)
-    return np.max(min_axis)
+def min_max():
+    X, _ = map(int, input().split())
+    array = []
+    for _ in range(X):
+        row = list(map(int, input().split()))
+        array.append(row)
+    min_values = np.min(array, axis=1)
+    max_value = np.max(min_values)
+    return max_value
+
+result = min_max()
+logging.debug(result)
+
+
 
 
