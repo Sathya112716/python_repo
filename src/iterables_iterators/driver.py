620170ad187aa3313e8d5fb9591ceda3e21414f8
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 22:36:56 +0530

    to calculate the probability of selecting at least one unique letter from a given list

--- src/iterables_iterators/driver.py
+++ src/iterables_iterators/driver.py
@@ -1,11 +1,3 @@
-import logging
-
 from util import probability_of_letter
 
-
-length = int(input())
-letters = input().split()
-num_indices = int(input())
-
-rslt = probability_of_letter(length, letters, num_indices)
-logging.debug(rslt)
+probability_of_letter()
--- src/iterables_iterators/util.py
+++ src/iterables_iterators/util.py
@@ -1,8 +1,19 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def probability_of_letter(length, letters, num_indices):
-    total_indices = len(letters)
-    count_letter = letters.count('a')
-    probability = 1 - ((1 - count_letter / total_indices) ** num_indices)
-    return round(probability, 4)
+def probability_of_letter():
+    n = int(input())
+    letters = input().split()
+    k = int(input())
+
+    indices = list(range(1, n + 1))
+    total_combinations = len(indices) ** k
+    combinations_with_letter = 0
+
+    for i in range(1, k + 1):
+        combinations_with_letter += len(set(letters[:i]))
+
+    probability = combinations_with_letter / total_combinations
+    return probability
+probability = probability_of_letter()
+logging.debug("{:.4f}".format(probability))
