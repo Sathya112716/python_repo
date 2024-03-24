726b13954ec38df3545453a978f835eab8186b9f
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:17:59 +0530

    test cases for mean var std calculation statisti

--- test/mean_var_std/test.py
+++ test/mean_var_std/test.py
@@ -1,13 +1,39 @@
-import unittest 
-from python_repo.src..mean_var_std.util import calculate_statistics
+import unittest
+from python_repo.src.mean_var_std.util import calculate_statistics
 
-class Testcalculatestatistics(unitTest.TestCase):
-  def test_calculate_statistics(self):
-    mean,var,std=calculate_statistics()
-    actual_input=[[1,2,3],[4,5,6]]
-    expected_mean=[2.0,5.0]
-    expected_var=[2.0,2.0,2.0]
-    expected_std=1.707
-    self.asserEqual(actual_input.tolist(),expected_mean)
-    self.asserEqual(actual_input.tolist(),expected_var)
-    self.asserEqual(actual_input.tolist(),expected_std)
+class MyTestCase(unittest.TestCase):
+    def test1_find_the_determinant_value(self):
+        # input=2 2
+        #       3 1
+        #       4 5
+        actual_mean, actual_var, actual_std = calculate_statistics()
+        expected_mean = [1.5, 3.5]
+        expected_var = [0.25, 0.25]
+        expected_std = [0.5, 0.5]
+        for actual, expected in zip((actual_mean, actual_var, actual_std), (expected_mean, expected_var, expected_std)):
+            for a, e in zip(actual, expected):
+                self.assertEqual(a, e)
+
+    def test2_find_the_determinant_value(self):
+        # input=2 2
+        #       4 5
+        #       6 7
+        actual_mean, actual_var, actual_std = calculate_statistics()
+        expected_mean = [4.5, 6.5]
+        expected_var = [0.25, 0.25]
+        expected_std = [0.5, 0.5]
+        for actual, expected in zip((actual_mean, actual_var, actual_std), (expected_mean, expected_var, expected_std)):
+            for a, e in zip(actual, expected):
+                self.assertEqual(a, e)
+    def test3_find_the_determinant_value(self):
+        # input=1 1
+        #       2 3
+        actual_mean, actual_var, actual_std = calculate_statistics()
+        expected_mean = [2.5]
+        expected_var = [0.25]
+        expected_std = [0.5]
+        for actual, expected in zip((actual_mean, actual_var, actual_std), (expected_mean, expected_var, expected_std)):
+            for a, e in zip(actual, expected):
+                self.assertEqual(a, e)
+if __name__ == '__main__':
+    unittest.main()
