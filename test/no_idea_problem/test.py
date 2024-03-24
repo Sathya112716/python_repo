b69546c869425d4ba909e6148ab3aaa61973da45
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:25:21 +0530

    test cases for no idea problem

--- test/no_idea_problem/test.py
+++ test/no_idea_problem/test.py
@@ -1,15 +1,32 @@
 import unittest
-from python_repo.src.No_idea_problem.util import calculate_happiness
+from python_repo.src.no_idea_problem.util import calculate_happiness
 
 class MyTestCase(unittest.TestCase):
     def test1(self):
-        actual_input=calculate_happiness()
-        expected_output=1
-        self.assertEqual=(actual_input,expected_output)
-        '''
-        sample input
-    3 2
-    1 5 3
-    3 1
-    5 7
-        '''
+        #n, m = 3, 2
+        #arr = [1, 5, 3]
+        #set_like = {3, 1}
+        #set_dislike = {5, 7}
+        actual_output = calculate_happiness()
+        expected_output = 1
+        self.assertEqual(actual_output, expected_output)
+    def test2(self):
+        #n, m = 3, 1
+        #arr = [1, 2, 3]
+        #set_like = {3, 2}
+        #set_dislike = {5, 4}
+        actual_output = calculate_happiness()
+        expected_output = 2
+        self.assertEqual(actual_output, expected_output)
+
+    def test3(self):
+        #n, m = 4,3
+        #arr = [5,3,2]
+        #set_like = {1,2}
+        #set_dislike = {6,5}
+        actual_output = calculate_happiness()
+        expected_output = 0
+        self.assertEqual(actual_output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
