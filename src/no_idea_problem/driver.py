a4f8d71c1728396b387a2331f5ae02bbf2addebe
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 23:05:39 +0530

    increments the happiness if an element is in the liked set and decrements it if it's in the disliked set

--- src/no_idea_problem/driver.py
+++ src/no_idea_problem/driver.py
@@ -1,11 +1,3 @@
-import logging
+
 from util import calculate_happiness
-
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-    arr = list(map(int, input().split()))
-    like_set = list(map(int, input().split()))
-    dislike_set = list(map(int, input().split()))
-
-    happy = calculate_happiness(n, m, arr, like_set, dislike_set)
-    logging.debug(happy)
+calculate_happiness()
--- src/no_idea_problem/util.py
+++ src/no_idea_problem/util.py
@@ -2,15 +2,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def calculate_happiness(n, m, arr, like_set, dislike_set):
+def calculate_happiness():
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    set_like = set(map(int, input().split()))
+    set_dislike = set(map(int, input().split()))
+
     happiness = 0
-    like_set = set(like_set)
-    dislike_set = set(dislike_set)
-
     for num in arr:
-        if num in like_set:
+        if num in set_like:
             happiness += 1
-        elif num in dislike_set:
+        elif num in set_dislike:
             happiness -= 1
-
     return happiness
+
+
+result = calculate_happiness()
+logging.debug(result)
+
