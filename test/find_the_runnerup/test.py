5c8f67e7076fc6495f1a7ed24898ef2933319b3e
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:10:25 +0530

    test case for second maximum in the input

--- test/find_the_runnerup/test.py
+++ test/find_the_runnerup/test.py
@@ -5,25 +5,25 @@ from python_repo.src.find_the_runnerup.util import find_runner_up_score
 
 class TestCalAvg(unittest.TestCase):
     def test_find_runner_up_score(self):
-        n = 3
-        scores = [4,6,1,7,9]
-        actualoutput = find_runner_up_score(scores)
-        expectedoutput = 7
-        self.assertEqual(actualoutput, expectedoutput)
+        #n = 5
+        #scores = [4,6,1,7,9]
+        actual_output = find_runner_up_score()
+        expected_output = 7
+        self.assertEqual(actual_output, expected_output)
 
     def test1_find_the_runnerup(self):
-        n = 6
-        scores=[8,4,5,2,9,8]
-        actualoutput = find_runner_up_score(scores)
-        expectedoutput = 8
-        self.assertEqual(actualoutput, expectedoutput)
+        #n = 6
+        #scores=[8,4,5,2,9,3]
+        actual_output = find_runner_up_score()
+        expected_output = 8
+        self.assertEqual(actual_output, expected_output)
 
     def test2_find_the_runnerup(self):
-        n = 3
-        scores = [4,1,5,8,10]
-        actualoutput = find_runner_up_score(scores)
-        expectedoutput = 8
-        self.assertEqual(actualoutput, expectedoutput)
+        #n = 3
+        #scores = [4,1,5]
+        actual_output = find_runner_up_score()
+        expected_output =4
+        self.assertEqual(actual_output, expected_output)
 
 
 if __name__ == '__main__':
