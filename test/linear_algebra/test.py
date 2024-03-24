240512bf5a2a859c9764fe727aee54bf6e47f21d
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:16:58 +0530

    test cases for linear algebra

--- test/linear_algebra/test.py
+++ test/linear_algebra/test.py
@@ -3,18 +3,25 @@ from python_repo.src.linear_algebra.util import calculate_determinant
 
 class MyTestCase(unittest.TestCase):
     def test1_find_the_determinant_value(self):
-        N = 2
-        matrix_value = [[1.1, 1.1], [1.1, 1.1]]
-        result = calculate_determinant(N, matrix_value)
-        self.assertEqual(result, 0.0)
+        # N = 2
+        # matrix_value = [[1.1, 1.1], [1.1, 1.1]]
+        actual_output = calculate_determinant()
+        expected_output = 0.0
+        self.assertAlmostEqual(actual_output, expected_output)
 
-    def test_find_the_determinant_value1(self):
-        N = 2
-        matrix_value = [[2.2, 2.2], [2.2, 2.2]]
-        result = calculate_determinant(N, matrix_value)
-        self.assertEqual(result, 0.0)
+    def test2_find_the_determinant_value(self):
+        # N = 2
+        # matrix_value = [[1.1, 1.2], [1.1, 1.1]]
+        actual_output = calculate_determinant()
+        expected_output = 0.0
+        self.assertAlmostEqual(actual_output, expected_output)
 
-
+    def test3_find_the_determinant_value(self):
+        # N = 2
+        # matrix_value = [[1.2, 1.2], [1.1,1.1]]
+        actual_output = calculate_determinant()
+        expected_output = 0.0
+        self.assertAlmostEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
