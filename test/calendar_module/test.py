28776a5097198d8c99f90047cd2146ed0d57b397
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:05:10 +0530

    test case for calendar_module

--- test/calendar_module/test.py
+++ test/calendar_module/test.py
@@ -1,23 +1,26 @@
 
 import unittest
-from python_repo.src.calendar_module.util import calendar_module
+from python_repo.src.calendar_module.util import find_day
 
 
 class TestCalAvg(unittest.TestCase):
-    def test(self):
-        actualoutput = calendar_module("28 12 2022")
-        expectedoutput = "WEDNESDAY"
-        self.assertEqual(actualoutput, expectedoutput)
+    def test1(self):
+        #date_input="03 22 2024"
+        actual_output= find_day()
+        expected_output = "FRIDAY"
+        self.assertEqual(actual_output, expected_output)#Using the calendar module calculatd the expected output
 
-    def test1_mutate_string(self):
-        actualoutput = calendar_module("26 07 2021")
-        expectedoutput = "MONDAY"
-        self.assertEqual(actualoutput, expectedoutput)
+    def test2(self):
+        #date_input="03 11 2024"
+        actual_output = find_day()
+        expected_output = "MONDAY"
+        self.assertEqual(actual_output, expected_output)
 
-    def test_mutate_string(self):
-        actualoutput = calendar_module("12 10 2020")
-        expectedoutput = "MONDAY"
-        self.assertEqual(actualoutput, expectedoutput)
+    def test3(self):
+        #date_input="03 03 2024"
+        actual_output = find_day()
+        expected_output = "SUNDAY"
+        self.assertEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
