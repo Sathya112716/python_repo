096f3df5e466ae77184f80533bf4043fd50c05bc
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-24 22:33:05 +0530

    use numpy module and calculate the rounded value of the array

--- src/floor_ceil_rint/driver.py
+++ src/floor_ceil_rint/driver.py
@@ -1,10 +1,4 @@
-import logging
-import numpy as np
-from util import floor_ceil_rint_calculate
 
-arr = np.array(input().split(), float)
-floor_result, ceil_result, rint_result = floor_ceil_rint_calculate(arr)
+from util import print_floor_ceil_rint
+print_floor_ceil_rint()
 
-logging.debug(floor_result)
-logging.debug(ceil_result)
-logging.debug(rint_result)
--- src/floor_ceil_rint/util.py
+++ src/floor_ceil_rint/util.py
@@ -1,11 +1,20 @@
 import logging
-import numpy as np
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-def floor_ceil_rint_calculate(arr):
-    np.set_printoptions(sign=' ')
-    floor_result = np.floor(arr)
-    ceil_result = np.ceil(arr)
-    rint_result = np.rint(arr)
-    return floor_result, ceil_result, rint_result
+import numpy as np
+
+def print_floor_ceil_rint():
+    arr = np.array(list(map(float, input().split())))
+    floor_arr = np.floor(arr)
+    ceil_arr = np.ceil(arr)
+    rint_arr = np.rint(arr)
+    return floor_arr, ceil_arr, rint_arr
+
+floor_arr, ceil_arr, rint_arr = print_floor_ceil_rint()
+logging.debug(floor_arr)
+logging.debug(ceil_arr)
+logging.debug(rint_arr)
+
+
 
 
