317181b6671a276dd16791b0e12df7a2675a9f6d
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 00:24:19 +0530

    test cases for mutate string

--- test/mutate_string/test.py
+++ test/mutate_string/test.py
@@ -3,8 +3,23 @@ import logging
 from python_repo.src.mutate_string.util import mutate_string
 class MyTestCase(unittest.TestCase):
     def test1_mutate_string(self):
-        self.assertEqual(mutate_string("sathya",2,"u"),"suthya") ##second position is mutated)
+        #string="priya
+        #p,c=3 t
+        actual_output=mutate_string()
+        expected_output='prita'
+        self.assertEqual(actual_output,expected_output)
+    def test2_mutate_string(self):
+        #string=lalitha
+        #p,c=4 s
+        actual_output=mutate_string()
+        expected_output='lalisha'
+        self.assertEqual(actual_output,expected_output)
 
-
+    def test3_mutate_string(self):
+        #string=ravi
+        #p,c=1 c
+        actual_output=mutate_string()
+        expected_output='rcvi'
+        self.assertEqual(actual_output,expected_output)
 if __name__ == '__main__':
     unittest.main()
