5694a72129c09827fcea7aefe9583c54be14bc67
Sathya112716 <sathyapriya112716@gmail.com>
2024-04-10 18:29:58 +0530

    For the given input and matrix calculate the output

--- src/find_the_percentage_problem/util.py
+++ src/find_the_percentage_problem/util.py
@@ -2,17 +2,14 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def calc_average(n,student_input,query_name):
-    student_marks={}
-
+def calc_average():
+    n = int(input())
+    student_marks = {}
     for _ in range(n):
-        name, *marks = student_input[_].split()
-        scored_value= list(map(float, marks))
-        student_marks[name] = scored_value
-
-    if query_name in student_marks:
-        average = sum(student_marks[query_name]) / len(student_marks[query_name])
-        return f"{average:.2f}"
-    else:
-        return "Student not there"
-
+        name, *marks = input().split()
+        student_marks[name] = list(map(float, marks))
+    query_name = input()
+    marks = student_marks.get(query_name)
+    average = sum(marks) / len(marks)
+    result = "{:.2f}".format(average)
+    logging.debug(result)
